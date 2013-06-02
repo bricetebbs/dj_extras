a91a630c3f87f4184f9247541a2b2f2b40722517
Brice Tebbs <brice@bricetebbs.com>
2013-06-02 16:43:43 -0700

    some new forms defaults

--- forms.py
+++ forms.py
@@ -1,24 +1,61 @@
 
 from django.forms.widgets import TextInput
-from django.db.models import PositiveIntegerField
+from django.db.models import PositiveIntegerField, EmailField, URLField, TimeField, DateField, DateTimeField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django import forms
 
 import models
+import datetime
 
 
-class PositiveNumberWidget(TextInput):
-    """
-        A widget for entering positive numbers. Using html5 'number'
-    """
+class PositiveIntegerWidget(TextInput):
     input_type = 'number'
     def __init__(self, *args, **kwargs):
         if not 'attrs' in kwargs:
             kwargs['attrs'] = dict(min=0)
         else:
             kwargs['attrs'].setdefault('min', 0)
+        super(PositiveIntegerWidget, self).__init__(*args, **kwargs)
 
-        super(PositiveNumberWidget, self).__init__(*args, **kwargs)
+
+class EmailWidget(TextInput):
+    input_type = 'email'
+    def __init__(self, *args, **kwargs):
+        super(EmailWidget, self).__init__(*args, **kwargs)
+
+
+class URLWidget(TextInput):
+    input_type = 'url'
+    def __init__(self, *args, **kwargs):
+        super(URLWidget, self).__init__(*args, **kwargs)
+
+
+class DateWidget(TextInput):
+    input_type = 'text'
+    def __init__(self, *args, **kwargs):
+        if not 'attrs' in kwargs:
+            kwargs['attrs'] = {'class' : 'datepicker',
+                                         'bs-datepicker' : '1',
+                                         'data-date-format' :"mm/dd/yyyy"}
+        super(DateWidget, self).__init__(*args, **kwargs)
+
+    def render(self, name, value, attrs=None):
+        if isinstance(value, datetime.date):
+            value=value.strftime("%d/%m/%Y")
+        return super(DateWidget, self).render(name, value, attrs)
+
+
+class TimeWidget(TextInput):
+    input_type = 'time'
+    def __init__(self, *args, **kwargs):
+        super(TimeWidget, self).__init__(*args, **kwargs)
+
+
+class DateTimeWidget(TextInput):
+    input_type = 'datetime'
+    def __init__(self, *args, **kwargs):
+
+        super(DateTimeWidget, self).__init__(*args, **kwargs)
 
 
 class PercentageWidget(TextInput):
@@ -36,8 +73,21 @@ class PercentageWidget(TextInput):
             for x in (('min', 0), ('max', 100)):
                 kwargs['attrs'].setdefault(x[0], x[1])
 
-
         super(PercentageWidget, self).__init__(*args, **kwargs)
+
+
+forms.ModelChoiceField
+
+class DateFormField(forms.DateField):
+    """
+    A custom formfield we are going to use because we need to change the acceptable input formats
+    """
+    widget = DateWidget
+
+    def __init__(self, *args, **kwargs):
+        super(DateFormField, self).__init__(*args, **kwargs)
+        self.input_formats = ("%m/%d/%Y",) + (self.input_formats,)
+
 
 class PercentageField(forms.FloatField):
     def __init__(self, *args, **kwargs):
@@ -73,8 +123,6 @@ class SliderField(forms.FloatField):
         super(SliderField, self).__init__(*args, **kwargs)
 
 
-
-
 def custom_formfield_callback(f, **kwargs):
     """
     Applies our own assignment of widgets etc when using model forms
@@ -97,13 +145,57 @@ def custom_formfield_callback(f, **kwargs):
             elif isinstance(v, MinValueValidator):
                 attrs['min'] = v.limit_value
         if 'max' in attrs:
-            return f.formfield(widget=PositiveNumberWidget(attrs=attrs), **kwargs)
+            return f.formfield(widget=PositiveIntegerWidget(attrs=attrs), **kwargs)
         
 
     #
     # Fallthrough and just do the default
     #
     return f.formfield(**kwargs)
+
+
+
+def angular_formfield_callback(f, **kwargs):
+    """
+    Applies our own assignment of widgets etc when using model forms
+    """
+    if isinstance(f, PositiveIntegerField):
+        field = f.formfield(widget = PositiveIntegerWidget(), **kwargs)
+    elif isinstance(f, EmailField):
+        field = f.formfield(widget = EmailWidget, **kwargs)
+    elif isinstance(f, URLField):
+        field = f.formfield(widget = URLWidget, **kwargs)
+    elif isinstance(f, DateField):
+        defaults = dict(form_class = DateFormField)
+        defaults.update(kwargs)
+        field = f.formfield(**defaults)
+        field.widget.attrs['ng-model'] = 'datepicker.dj_' + f.name
+        field.widget.attrs['dj_init'] = field.__class__.__name__.lower()
+    elif isinstance(f, TimeField):
+        field = f.formfield(widget = TimeWidget, **kwargs)
+    elif isinstance(f, DateTimeField):
+        field = f.formfield(widget = DateTimeWidget, **kwargs)
+    else:
+        # If we don't have a special one just let django pick for us
+        field = f.formfield(**kwargs)
+
+    if field:  # sometimes there is no field...
+        for v in f.validators:
+            if isinstance(v, MaxValueValidator):
+                field.widget.attrs['max'] = v.limit_value
+            elif isinstance(v, MinValueValidator):
+                field.widget.attrs['min'] = v.limit_value
+
+
+        if 'ng-model' not in field.widget.attrs:
+            # set up a model for this so ng can do stuff with it
+            field.widget.attrs['ng-model'] = 'dj_%s' % f.name
+        if 'dj_init' not in field.widget.attrs:
+            # set the model name as an attribute of the init directive so we can set up the scope
+            field.widget.attrs['dj_init'] = field.__class__.__name__.lower()
+
+    return field
+
 
 def get_form_class_for_class(klass):
     """
